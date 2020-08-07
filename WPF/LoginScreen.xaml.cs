0d85133bdb264e42eacecdb04925f7363ce67fb5
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-08-07 00:29:20 -0500

    Criando exemplo com WPF.Net Core

---
+++ WPF/LoginScreen.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace WPF
+{
+	public partial class LoginScreen : Window
+	{
+		public LoginScreen(SplashScreen splash)
+		{
+			InitializeComponent();
+			splash.Close();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(emailField.Text) || string.IsNullOrEmpty(passwordField.Password))
+			{
+				MessageBox.Show("Voce precisa preencher ambos os campos!");
+				return;
+			}
+
+			MessageBox.Show($"{emailField.Text} voce logou com sucesso!");
+		}
+	}
+}
---
+++ WPF/SplashScreen.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WPF
+{
+	public partial class SplashScreen : Window
+	{
+		public SplashScreen()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var login = new LoginScreen(this);
+			login.Show();
+		}
+	}
+}
