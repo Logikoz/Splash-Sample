4bd1385444d61dfe109977468b712980ee567f0f
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-08-06 23:47:49 -0500

    Add project files.

---
+++ WindowsForms/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+	public partial class SplashScreen : Form
+	{
+		public SplashScreen()
+		{
+			InitializeComponent();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+		}
+	}
+}
